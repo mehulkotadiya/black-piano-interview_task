ab5c1200eb6fbe970728de4a6abbf5230a3264b5
mehulkotadiya <90110437+mehulkotadiya@users.noreply.github.com>
2024-04-24 15:09:24 +0530

    Update script according to Joe response

--- ga4apidata.py
+++ ga4apidata.py
@@ -8,39 +8,36 @@ from google.analytics.data_v1beta.types import (
     RunReportRequest,
 )
 from google.oauth2 import service_account
-import pandas as pd
+import csv
 
 # Set credentials and define the right GA4 property
 
 credentials = service_account.Credentials.from_service_account_file('/content/gcp-credentials.json') 
 client = BetaAnalyticsDataClient(credentials=credentials)
-property_id="305482976"
+property_id="309591865"
 
 # Get the data from the API
 
 request = RunReportRequest(
     property=f"properties/{property_id}",
     dimensions=[Dimension(name="city"), Dimension(name="country"),Dimension(name="date")],
-    metrics=[Metric(name="totalUsers"), Metric(name="sessions")],
-    date_ranges=[DateRange(start_date="2023-09-20", end_date="2024-04-22")],
+    metrics=[Metric(name="activeUsers"), Metric(name="sessions")],
+    date_ranges=[DateRange(start_date="2023-01-01", end_date="2023-12-31")],
     )
 
 response = client.run_report(request)
 
-# Turn the raw data into a Table
+# Turn the raw data into a csv file
 
-def ga4_result_to_df(response):
-    result_dict = {dimensionHeader.name: [] for dimensionHeader in response.dimension_headers}
-    result_dict.update({metricHeader.name: [] for metricHeader in response.metric_headers})
-
+with open('ga4_data.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    
+    # Writing headers
+    headers = [dimension.name for dimension in response.dimension_headers] + [metric.name for metric in response.metric_headers]
+    csvwriter.writerow(headers)
+    
+    # Writing rows
     for row in response.rows:
-        for i, dimension_value in enumerate(row.dimension_values):
-            dimension_name = response.dimension_headers[i].name
-            result_dict[dimension_name].append(dimension_value.value)
-        for i, metric_value in enumerate(row.metric_values):
-            metric_name = response.metric_headers[i].name
-            result_dict[metric_name].append(metric_value.value)
-
-    return pd.DataFrame(result_dict)
-
-df = ga4_result_to_df(response)
+        dimension_values = [dimension.value for dimension in row.dimension_values]
+        metric_values = [metric.value for metric in row.metric_values]
+        csvwriter.writerow(dimension_values + metric_values)
